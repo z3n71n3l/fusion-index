db7d826e9ed246e954d94624e993539c745b817b
Tristan Seligmann <mithrandi@mithrandi.net>
2015-08-28 12:49:02 +0200

    Ditch stateful thing.
    --HG--
    branch : 1-lookup-index

--- fusion_index/test/test_lookup.py
+++ fusion_index/test/test_lookup.py
@@ -1,10 +1,8 @@
-from axiom.attributes import ConstraintError
 from axiom.store import Store
 from hypothesis import given
-from hypothesis.stateful import Bundle, RuleBasedStateMachine, rule
 from hypothesis.strategies import binary, lists, text, tuples
 from testtools import TestCase
-from testtools.matchers import Equals, raises
+from testtools.matchers import Equals
 
 from fusion_index.lookup import LookupEntry
 
@@ -21,32 +19,11 @@ class LookupTests(TestCase):
         """
         s = Store()
         def _tx():
+            d = {}
             for e, t, k, v in values:
                 LookupEntry.set(s, e, t, k, v)
+                d[(e, t, k)] = v
                 self.assertThat(LookupEntry.get(s, e, t, k), Equals(v))
-        s.transact(_tx)
-
-
-
-class LookupStateMachine(RuleBasedStateMachine):
-    stores = Bundle('stores')
-
-    @rule(target=stores)
-    def newStore(self):
-        return Store(), {}
-
-
-    @rule(s=stores, e=axiom_text(), t=axiom_text(), k=axiom_text(), v=binary())
-    def insert(self, s, e, t, k, v):
-        s, d = s
-        s.transact(LookupEntry.set, s, e, t, k, v)
-        d[(e, t, k)] = v
-
-
-    @rule(s=stores)
-    def compare(self, s):
-        s, d = s
-        def _tx():
             for (e, t, k), v in d.iteritems():
-                assert LookupEntry.get(e, t, k) == v
+                self.assertThat(LookupEntry.get(s, e, t, k), Equals(v))
         s.transact(_tx)
