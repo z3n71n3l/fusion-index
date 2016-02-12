92031812b77479fe9a3dbd3ca512ba97e700384e
Tristan Seligmann <mithrandi@mithrandi.net>
2016-02-12 16:51:57 +0200

    Fix test model to be case-insensitive.

--- fusion_index/test/test_lookup.py
+++ fusion_index/test/test_lookup.py
@@ -1,6 +1,8 @@
+import string
+
 from axiom.store import Store
 from hypothesis import given
-from hypothesis.strategies import binary, lists, text, tuples, characters
+from hypothesis.strategies import binary, characters, lists, text, tuples
 from testtools import TestCase
 from testtools.matchers import Equals
 
@@ -15,6 +17,18 @@ def axiom_text():
         average_size=5)
 
 
+_lower_table = dict(
+    zip(map(ord, string.uppercase.decode('ascii')),
+        map(ord, string.lowercase.decode('ascii'))))
+
+
+def _lower(s):
+    """
+    Lowercase only ASCII characters, like SQLite NOCASE.
+    """
+    return s.translate(_lower_table)
+
+
 class LookupTests(TestCase):
     @given(lists(tuples(axiom_text(), axiom_text(), axiom_text(), binary())))
     def test_inserts(self, values):
@@ -26,7 +40,7 @@ class LookupTests(TestCase):
             d = {}
             for e, t, k, v in values:
                 LookupEntry.set(s, e, t, k, v)
-                d[(e, t, k)] = v
+                d[(_lower(e), _lower(t), _lower(k))] = v
                 self.assertThat(LookupEntry.get(s, e, t, k), Equals(v))
             for (e, t, k), v in d.iteritems():
                 self.assertThat(LookupEntry.get(s, e, t, k), Equals(v))
